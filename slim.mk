b718db04db376e2e20fb025d7586d38ba68c66d6
shubham01 <shubhmgajra@gmail.com>
2015-03-21 16:51:21 +0530

    Update and rename cm.mk to slim.mk

--- slim.mk
+++ slim.mk
@@ -1,10 +1,15 @@
+# Boot animation
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_condor
+PRODUCT_NAME := slim_condor
 PRODUCT_RELEASE_NAME := MOTO E
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
