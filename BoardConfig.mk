e4ce66671dc3b056be136dbca655a8f3372d19ed
Luca Stefani <luca.stefani.ge1@gmail.com>
2015-10-29 10:02:38 -0700

    Condor: remove custom mkbootimg
    The CM12.1 build process now has integrated support for
    separated dt images.
    
    Change-Id: I8946f966916f28d9de174e6255eb10944ce59059

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,16 +40,13 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8610
 TARGET_KERNEL_CONFIG := cm_condor_defconfig
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags vmalloc=400M
-
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-
 
 WLAN_MODULES:
 	mkdir -p $(KERNEL_MODULES_OUT)/pronto
