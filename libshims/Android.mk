ec04a5e131a957882c5db169c99e0e8b76c3a55d
Christopher R. Palmer <crpalmer@gmail.com>
2015-11-17 14:00:28 -0500

    Condor: Shim libqc-opt rather than hexediting it.
    Change-Id: I4e57ea5746b771bdac11f7d2e48a13f0e8077f2f

---
+++ libshims/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+# libqc-opt
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+     icu53.c
+
+LOCAL_SHARED_LIBRARIES := libicuuc libicui18n
+LOCAL_MODULE := libshim_qcopt
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
