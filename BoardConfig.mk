5f28336a51c67ddf63ba39708360e5345e62dff7
Ashwin <ashwinr64@gmail.com>
2015-11-18 09:16:40 +0530

    Condor:Dex-preopt on all builds.Takes years to boot.
    Change-Id: Iaabe029a29afce813860fa2a2da4060af778b828

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,13 +152,11 @@ MALLOC_IMPL := dlmalloc
 
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
       WITH_DEXPREOPT_BOOT_IMG_ONLY := false
     endif
   endif
-endif
 
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
