4fd8d1404e2018ef60d9a2f16031b3760e0cab72
Michael Bestas <mikeioannina@gmail.com>
2015-12-01 22:08:11 -0500

    doze: Use ScreenType util from cmsdk
    Change-Id: Ie2c2191dc338342dae7ca3bbde97d9d2c7084b29

--- doze/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
+++ doze/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -16,7 +16,7 @@
 
 package com.cyanogenmod.settings.device;
 
-import com.android.internal.util.cm.ScreenType;
+import org.cyanogenmod.internal.util.ScreenType;
 
 import android.app.ActionBar;
 import android.os.Bundle;
