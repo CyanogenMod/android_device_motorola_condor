8e24f41a69a1faa35775410be344f6389d9d36c6
Ashwin <ashwinr64@gmail.com>
2015-12-06 00:32:23 +0530

    Condor: Commonize stuff for unifying otus and condor
    Change-Id: I0764617307eb13ea2f4a942b3e2f8d07472fef3f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,110 +18,13 @@
 
 LOCAL_PATH := device/motorola/condor
 
-BOARD_VENDOR := motorola-qcom
-
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
-
-# Platform
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno302
-TARGET_BOARD_PLATFORM := msm8610
-
-USE_CLANG_PLATFORM_BUILD := true
-
-# Architecture
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_MEMCPY_BASE_OPT_DISABLE := true
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := krait
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := MSM8610
-TARGET_NO_BOOTLOADER := true
-
-# Charger
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/mmi_lpm/lpm_mode
-
 # Kernel
-BOARD_DTBTOOL_ARGS := -v2
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_CONFIG := cm_condor_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8610
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x37 utags.blkdev=/dev/block/bootdevice/by-name/utags vmalloc=400M androidboot.selinux=permissive
-
-WLAN_MODULES:
-	mkdir -p $(KERNEL_MODULES_OUT)/pronto
-	mv $(KERNEL_MODULES_OUT)/wlan.ko $(KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
-	ln -sf /system/lib/modules/pronto/pronto_wlan.ko $(TARGET_OUT)/lib/modules/wlan.ko
-
-TARGET_KERNEL_MODULES += WLAN_MODULES
-
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
-# GPS
-TARGET_NO_RPC := true
-
-# Global flags
-COMMON_GLOBAL_CFLAGS += -DMOTOROLA_UIDS -DQCOM_HARDWARE -DCAMERA_VENDOR_L_COMPAT
-TARGET_NR_SVC_SUPP_GIDS := 32
-
-# Display
-USE_OPENGL_RENDERER := true
-TARGET_USES_ION := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-MAX_EGL_CACHE_SIZE := 2048*1024
-TARGET_USE_COMPAT_GRALLOC_PERFORM := true
-
-# Wifi
-BOARD_HAS_QCOM_WLAN := true
-BOARD_HAS_QCOM_WLAN_SDK := true
-BOARD_WLAN_DEVICE := qcwcn
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME := "wlan"
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-
-# Audio
-BOARD_USES_ALSA_AUDIO := true
-AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
-AUDIO_FEATURE_ENABLED_NEW_SAMPLE_RATE := true
-AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
-USE_CUSTOM_AUDIO_POLICY := 1
-
-# FM
-TARGET_QCOM_NO_FM_FIRMWARE := true
-
-# Qualcomm support
-BOARD_USES_QCOM_HARDWARE := true
-BOARD_USES_QC_TIME_SERVICES := true
-TARGET_RIL_VARIANT := caf
-
-# Hardware tunables framework
-BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw/
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
-BLUETOOTH_HCI_USE_MCT := true
-
-# Build
-TARGET_SYSTEMIMAGE_USE_SQUISHER := true
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Storage & partiiton
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 10444800
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10526720
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 939524096
@@ -130,42 +33,11 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 476184576
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Vold
-BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_VOLD_MAX_PARTITIONS := 40
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := xt1021,xt1022,xt1023,condor_umts,condor_umtsds,condor
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_condor.cpp
-TARGET_UNIFIED_DEVICE := true
-
-# Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.qcom
-BOARD_HAS_NO_SELECT_BUTTON := true
-HAVE_SELINUX := true
-
-# SELinux
-include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     device/motorola/condor/sepolicy
-
-MALLOC_IMPL := dlmalloc
-
-# Enable dex-preoptimization to speed up first boot sequence
-ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_ONLY := false
-    endif
-  endif
-endif
-
-# Include an expanded selection of fonts
-EXTENDED_FONT_FOOTPRINT := true
--- device.mk
+++ device.mk
@@ -21,8 +21,6 @@
 
 LOCAL_PATH := device/motorola/condor
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # Ramdisk
  PRODUCT_COPY_FILES += \
      $(call find-copy-subdir-files,*,${LOCAL_PATH}/ramdisk,root)
@@ -30,40 +28,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Prebuilt
 PRODUCT_COPY_FILES += \
      $(call find-copy-subdir-files,*,${LOCAL_PATH}/prebuilt/system,system)
-
-# These are the hardware-specific features
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
-    frameworks/native/data/etc/android.software.print.xml:system/etc/permissions/android.software.print.xml \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
-
-PRODUCT_TAGS += dalvik.gc.type-precise
-
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
-DEVICE_PACKAGE_OVERLAYS := \
-    $(LOCAL_PATH)/overlay
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 960
-TARGET_SCREEN_WIDTH := 540
 
 PRODUCT_PACKAGES += \
     gralloc.msm8610 \
@@ -95,109 +59,13 @@ PRODUCT_PACKAGES += \
     libboringssl-compat \
     libshim_qcopt
 
-# Filesystem
-PRODUCT_PACKAGES += \
-    setup_fs
-
-# Qcom SoftAP & wifi
-PRODUCT_PACKAGES += \
-    libQWiFiSoftApCfg
-
-# Motorola
-PRODUCT_PACKAGES += \
-    charge_only_mode
-
-# Misc
-PRODUCT_PACKAGES += \
-    libxml2
-
 # Motorola
 PRODUCT_PACKAGES += \
     libmoto
 
-# OMX
-PRODUCT_PACKAGES += \
-    libdashplayer \
-    libextmedia_jni \
-    libOmxAacEnc \
-    libOmxAmrEnc \
-    libOmxCore \
-    libOmxEvrcEnc \
-    libOmxQcelp13Enc \
-    libOmxVdec \
-    libOmxVenc \
-    libOmxVidcCommon \
-    libqcmediaplayer \
-    libstagefrighthw \
-    qcmediaplayer
-
-PRODUCT_BOOT_JARS += \
-    qcmediaplayer
-
-PRODUCT_PACKAGES += \
-    wlan_module_symlink \
-    wlan_persist_symlink \
-    wcnss_service
-
-PRODUCT_PACKAGES += \
-    librs_jni \
-    com.android.future.usb.accessory
-
-# Ebtables
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes \
-    libebtc
-
-# FM
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
-
-# Lights
-PRODUCT_PACKAGES += \
-    lights.msm8610
-
-# GPS
-PRODUCT_PACKAGES += \
-    gps.msm8610
-
 # Stlport
 PRODUCT_PACKAGES += \
     libstlport
-
-# Misc
-PRODUCT_PACKAGES += \
-    curl \
-    libbson \
-    libcurl \
-    tcpdump \
-    libcnefeatureconfig
-
-# Wifi
-PRODUCT_PACKAGES += \
-    hostapd_default.conf \
-    hostapd.accept \
-    hostapd.deny \
-    hostapd \
-    wpa_supplicant \
-    wpa_supplicant_overlay.conf \
-    p2p_supplicant_overlay.conf \
-    dhcpcd.conf \
-    wpa_supplicant.conf \
-    wlan_module_symlink \
-    wlan_persist_symlink \
-    wcnss_service \
-    libQWiFiSoftApCfg \
-    libqsap_sdk
-
-# Doze
-PRODUCT_PACKAGES += \
-    MotoDoze
-
-# MultiWindow ;p
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.debug.multi_window=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240
@@ -239,8 +107,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     kernel/motorola/msm8610/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     kernel/motorola/msm8610/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini
-
-# Inhert dalvik heap values from aosp
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-
-$(call inherit-product-if-exists, hardware/qcom/msm8x74/msm8x74.mk)
