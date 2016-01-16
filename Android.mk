26371083d231d2dfe4367b57463c84150c93de33
Ashwin <ashwinr64@gmail.com>
2016-01-16 09:38:21 -0800

    condor: It's 2016
    *Your works are highly respected Percy ,But when project is a sub-set of CyanogenMod ,it is better to have the licensing under CM.
    
    Change-Id: Ibff822fccec471d7ed2c4e32126befd9af61aed4

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 Prashant Gahlot (proxthehacker@gmail.com)
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter condor, $(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),condor)
 
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
